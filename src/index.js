044ea79f0e646a872bb02c185bb032b61b017b6f
MijbahMERN <misbhakhandoker@gmail.com>
2024-08-14 18:46:09 +0600

    spelling mistake

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // require('dotenv').config({path: "./env"})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import express from "express";
 dotenv.config({path:"./evn"})
 
 const Port = process.env.PORT || 8000
+
+const app = express()
 
 connectDB()
 
